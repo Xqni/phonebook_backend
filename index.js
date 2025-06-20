f24ff17fb23045c643e3f5bc5ce15c629d069cd0
Xqni <jxesuki@gmail.com>
2025-06-19 23:51:50 -0500

    add validation

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ const Person = require('./models/person')
 
 const app = express()
 
-app.use(express.json())
-app.use(cors())
 app.use(express.static('dist'))
+app.use(cors())
+app.use(express.json())
 
 
 // Define a custom token for request body
 morgan.token('body', (req) => {
     return req.method === 'POST' ? JSON.stringify(req.body) : '';
-});
+})
 
 // Use morgan with the custom token
 app.use(
@@ -23,15 +23,16 @@ app.use(
 )
 
 app.get('/', (request, response) => {
-    response.send('<h1>If you see this, I messed up!App</h1>')
+    response.send('<h1>If you see this, I messed up!</h1>')
 })
 
 app.get('/info', (request, response) => {
-    let startTime = Date()
-    response.send(`
-        <div>Phonebook has info for ${persons.length} people</div>
-        <div>${startTime}</div>
-        `)
+    const startTime = Date()
+    Person.find({}).then(result => {
+        const people = result
+        response.send(`<div>Phonebook currently has info about ${people.length} people</div>
+            <p>${startTime}</p>`)
+    })
 })
 
 app.get('/api/persons', (request, response) => {
@@ -40,26 +41,20 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(note => note.id === id)
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
 
-
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+        .catch(error => next(error))
 })
 
-const generateId = () => {
-    const maxId = persons.length > 0
-        ? Math.max(...persons.map(person => Number(person.id)))
-        : 0
-    return String(maxId + 1)
-}
-
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     const person = new Person({
@@ -67,17 +62,48 @@ app.post('/api/persons', (request, response) => {
         number: body.number,
     })
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+app.put('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            const number = request.body.number
+            person.number = number
+            person.save().then(savedPerson => {
+                response.json(savedPerson)
+            })
+        })
+        .catch(error => next(error))
 })
+
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+// Error handler middleware 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).send({ error: 'Name should be longer than 3 characters' })
+    }
+
+    next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3002
 app.listen(PORT, () => {
--- models/person.js
+++ models/person.js
@@ -17,9 +17,13 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-        name: String,
-        number: String,
-    })
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: String,
+})
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
